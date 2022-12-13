e338a3d8fd5d25f8e52e886b20e7c4159ce37c74
Basem Elsisy <basemsisy@gmail.com>
2022-12-13 18:41:12 +0200

    feat: setup playground for react package

---
+++ playground/react/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@dse/react";
+const App = () => {
+  return (
+    <div className="App">
+      <Button label="Example button" />
+    </div>
+  );
+};
+
+export default App;
