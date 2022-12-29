45d152a73bce969a358c65c507326ecfa4c0beda
Basem Elsisy <basemsisy@gmail.com>
2022-12-29 20:42:24 +0200

    fix conflicts

--- packages/react/src/atoms/Color/Color.tsx
+++ packages/react/src/atoms/Color/Color.tsx
@@ -1,13 +1,26 @@
 import React, { FC } from "react";
+import spacing from "../../foundation/spacing";
 
 interface ColorProps {
   hexCode: string;
-  width: string;
-  height: string;
+  width?: typeof spacing;
+  height?: typeof spacing;
 }
 
-const Color: FC<ColorProps> = ({ hexCode, width, height }) => {
-  return <div style={{ backgroundColor: hexCode, width, height }}></div>;
+const Color: FC<ColorProps> = ({
+  hexCode,
+  width = spacing.md,
+  height = spacing.md,
+}) => {
+  const className = `dse-width-${width} dse-height-${height}`;
+  return (
+    <div
+      style={{
+        backgroundColor: hexCode,
+      }}
+      className={className}
+    />
+  );
 };
 
 export default Color;
